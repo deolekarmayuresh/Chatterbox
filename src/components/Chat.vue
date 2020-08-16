1c1434b813106ea6119f2fc177d1d2f50e0f69e6
mayuresh deolekar <mayureshdeolekar.md@gmail.com>
2020-08-16 22:43:50 +0530

    chat.vue

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,6 +1,20 @@
 <template>
     <div class="chat container">
-        <h2>Chat {{ this.name }}</h2>
+        <h2 class="center blue-text">Chatterbox</h2>
+        <div class="card">
+            <div class="card-content">
+                <ul class="messages">
+                    <li>
+                        <span class="blue-text">Name</span>
+                        <span class="grey-text text-darken-3 message">Message</span>
+                        <span class="grey-text time">Time</span>  
+                    </li>
+                </ul>
+            </div>
+            <div class="card-action">
+                <input type="text">
+            </div>
+        </div>
     </div>
 </template>
 
@@ -18,5 +32,19 @@ export default {
 </script>
 
 <style>
+.chat h2{
+    font-size: 2.6em;
+    margin-bottom: 40px;
+}
+.chat span{
+    font-size: 1.4em;
+}
+.chat .message{
+    display: block;
     
+}
+.chat .time{
+    display: block;
+    font-size: 1.2em;
+}
 </style>

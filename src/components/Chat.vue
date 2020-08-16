9a867353619edf7c1740948c7598989d9a411e02
mayuresh deolekar <mayureshdeolekar.md@gmail.com>
2020-08-16 21:28:21 +0530

    chat.vue

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,15 +1,17 @@
 <template>
     <div class="chat container">
-        <h2>Chat</h2>
+        <h2>Chat {{ this.name }}</h2>
     </div>
 </template>
 
 <script>
 export default {
     name: 'Chat',
+    props: ['name'],
     data() {
         return {
             
+
         }
     },
 }

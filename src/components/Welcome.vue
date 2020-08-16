014c3128a4f7e640368bf53803d409e513d959b7
mayuresh deolekar <mayureshdeolekar.md@gmail.com>
2020-08-16 21:22:57 +0530

    index.js

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -6,6 +6,7 @@
         <form @submit.prevent='enterChat'>
           <label for="name">Enter your name: </label>
           <input type="text" name="name" v-model="name">
+          <p v-if="feedback" class="red-text"> {{ feedback }} </p>
           <button class="btn blue">Enter Chatterbox</button>
         </form>
       </div>
@@ -18,12 +19,18 @@ export default {
   name: 'Welcome',
   data () {
     return {
-      name: null
+      name: null,
+      feedback: null
     }
   },
   methods: {
     enterChat() {
-      console.log(this.name)
+      // console.log(this.name)
+      if (this.name) {
+        
+      }else {
+        this.feedback = 'Please enter your name to join'
+      }
     }
   }
 }

84715bfeb96de4a82b427e5d6a2679761e6715f4
mayuresh deolekar <mayureshdeolekar.md@gmail.com>
2020-08-16 19:20:43 +0530

    adding firebase sdk

---
+++ src/firebase/init.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAjCKqM8NvSVHznBcq2kH_7Fm0HWvEMWFo",
+    authDomain: "my-chatterbox.firebaseapp.com",
+    databaseURL: "https://my-chatterbox.firebaseio.com",
+    projectId: "my-chatterbox",
+    storageBucket: "my-chatterbox.appspot.com",
+    messagingSenderId: "1042592532740",
+    appId: "1:1042592532740:web:7e810e5e413cffec1c6851",
+    measurementId: "G-071WL4V2Q8"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();

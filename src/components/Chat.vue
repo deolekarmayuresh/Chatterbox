41333194e357594cb1cbabad64df6374cea3bd47
mayuresh deolekar <mayureshdeolekar.md@gmail.com>
2020-08-16 22:59:13 +0530

    chat.vue

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -12,19 +12,23 @@
                 </ul>
             </div>
             <div class="card-action">
-                <input type="text">
+                <NewMessage name='name' /> 
             </div>
         </div>
     </div>
 </template>
 
 <script>
+import NewMessage from '@/components/NewMessage'
+
 export default {
     name: 'Chat',
     props: ['name'],
+    components: {
+        NewMessage
+    },
     data() {
         return {
-            
 
         }
     },
